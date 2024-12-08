0b4e84cbf1c068e35550a5fd66575f48e64aea67
Gradlee <graydonsemail@gmail.com>
2024-12-08 12:23:46 +1100

    Day 1 complete with 2 stars

--- 2024/Day1/Part2.py
+++ 2024/Day1/Part2.py
@@ -6,7 +6,7 @@ text_file = open("input.txt","r")
 lines = text_file.readlines()
 
 # Initialising variables
-data = {"Left": [], "Right": [], "Distance": []}
+data = {"Left": [], "Right": [], "Similarity": []}
 
 for line in lines:
     # Replacing sequential whitespace with a comma delimiter
@@ -18,15 +18,13 @@ for line in lines:
     data["Left"].append(left)
     data["Right"].append(right)
 
-# Sort both the left and right columns to calculate the distance between them 
-data["Left"].sort()
-data["Right"].sort()
 
-# Calculate the difference for every sorted pair
-data["Distance"] = [abs(right - left) for left, right in zip(data["Left"], data["Right"])]
+
+# Calculate the similarity of each left element and the number of appearances in the right column
+data["Similarity"] = [left * data["Right"].count(left) for left in data["Left"]]
 
 # Sum the total distance
-totalDistance = sum(data["Distance"])
-print(totalDistance)
+Similarity = sum(data["Similarity"])
+print(Similarity)
 
 text_file.close()
